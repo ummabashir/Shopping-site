a752f804d62ac14bb4a59ea9c6a2f4a7a90225bd
khairbashir <khairiyyabashir87@gmail.com>
2022-04-19 15:23:11 +0100

    new changes

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ if (close) {
     })
 }
 
+//product img shop details
+let MainImg = document.getElementById('MainImg');
+
+let smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function(){
+    MainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = function(){
+    MainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = function(){
+    MainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = function(){
+    MainImg.src = smallImg[3].src;
+}
